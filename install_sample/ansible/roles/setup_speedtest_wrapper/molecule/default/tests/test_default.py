c550f3c9e8ec1fd5984ac55c173e8cff0c4d2d31
Eldio Santos <eldio.santos@m4u.com.br>
2021-05-16 21:14:19 -0300

    Commiting Ansible code to finish later

--- install_sample/ansible/roles/setup_speedtest_wrapper/molecule/default/tests/test_default.py
+++ install_sample/ansible/roles/setup_speedtest_wrapper/molecule/default/tests/test_default.py
@@ -13,7 +13,6 @@ def test_hosts_file(host):
 def test_app_user_ispresent(host):
     u = host.user("speedtest")
 
-    print(u)
     assert u.exists
     assert u.expiration_date is None
 
@@ -44,3 +43,34 @@ def test_app_timer_file_ispresent(host):
     assert f.group == "root"
 
     assert f.contains("OnCalendar=\\*\\-\\*\\-\\* \\*:\\*:00")
+
+
+def test_app_folder_ispresent(host):
+    f = host.file("/app/speedtest_wrapper")
+
+    assert f.exists
+    assert f.user == "speedtest"
+    assert f.group == "speedtest"
+    assert f.is_directory
+
+
+def test_cfg_file_ispresent(host):
+    f = host.file("/app/speedtest_wrapper/config.yml")
+
+    assert f.exists
+    assert f.user == "speedtest"
+    assert f.group == "speedtest"
+    assert f.is_file
+
+    assert f.contains("host: 127.0.0.1")
+    assert f.contains("port: 1883")
+    assert f.contains("qos: 1")
+
+
+def test_bin_file_ispresent(host):
+    f = host.file("/app/speedtest_wrapper/speedtest-wrapper-go")
+
+    assert f.exists
+    assert f.user == "speedtest"
+    assert f.group == "speedtest"
+    assert f.is_file
