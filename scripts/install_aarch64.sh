900fd92e86dc51891db260254e1ba00a6bc798e4
Eldio Santos <eldio.santos@m4u.com.br>
2021-10-12 13:22:32 -0300

    Added a build to aarch64 architecture

---
+++ scripts/install_aarch64.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if ! command -v jq& /dev/null
+then
+    echo "jq could not be found"
+    exit
+fi
+
+owner=eldius
+repo=speedtest-wrapper-go
+
+wget "$( curl https://api.github.com/repos/${owner}/${repo}/releases | jq -r '. | sort_by(.created_at) | last | .assets[] | select(.name | endswith(".aarch64")) | .browser_download_url' )"
+
+mv speedtest-wrapper* speedtest-wrapper
+sudo mv speedtest-wrapper /usr/bin
